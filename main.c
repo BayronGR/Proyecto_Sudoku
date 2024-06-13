bb33f6051a8db4879f4cfc5afc75d7ee65991fa9
juanpablodiaz22 <166567190+juanpablodiaz22@users.noreply.github.com>
2024-06-13 16:26:56 -0600

    Primera sección main.c
    Se añade la biblioteca y el archivo header.h. Además, se declara una función externa para ejecutar la interfaz gráfica y se muestra el menú principal con su dificultad para ser seleccionado

---
+++ main.c
@@ -0,0 +1,23 @@
+// main.c
+// Se incluye la biblioteca estandar y el archivo header.h
+#include <stdio.h>
+#include "game_logic.h"
+
+// Se declara una función externa para ejecutar la interfaz gráfica
+extern int run_interface(int argc, char **argv);
+
+// Aca se muestra el menú principal para seleccionar la dificultad del Sudoku.
+void show_main_menu() {
+    int difficulty; // Esta variable almacena la dificultad seleccionada
+
+    // Se imprimir el menú de selección de dificultad
+    printf("Seleccione la dificultad del Sudoku:\n");
+    printf("1. Sudoku fácil\n");
+    printf("2. Sudoku medio\n");
+    printf("3. Sudoku difícil\n");
+    printf("Opción: ");
+    scanf("%d", &difficulty); // Se lee la opción seleccionada por el usuario
+
+    selectSudoku(difficulty); // Se encarga de llamar a la función para seleccionar el sudoku según la dificultad
+    printSudoku();
+}
